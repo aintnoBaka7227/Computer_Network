669737d99839e1c20abc8fbfc4d3509736b79215
Phong <a1873825@adelaide.edu.au>
2025-03-23 16:57:20 +1030

    implement request to origin server, handle response and cache

--- Proxy.py
+++ Proxy.py
@@ -68,7 +68,7 @@ while True:
     # ~~~~ INSERT CODE ~~~~
     
     clientSocket, addr = tcpServerSocket.accept()
-    print('client address: ' + addr)
+    print('client address: ' + str(addr))
     
     # ~~~~ END CODE INSERT ~~~~
     print ('Received a connection')
@@ -196,12 +196,18 @@ while True:
       # Get the response from the origin server
       # ~~~~ INSERT CODE ~~~~
       
-      
+      originServerResponse = originServerSocket.recv(BUFFER_SIZE)
+      print("received origin server response: " + originServerResponse.decode('utf-8'))
       
       # ~~~~ END CODE INSERT ~~~~
 
       # Send the response to the client
       # ~~~~ INSERT CODE ~~~~
+      
+      clientSocket.sendall(originServerResponse)
+      print("origin server response sent to client")
+      print ('> ' + originServerResponse)
+      
       # ~~~~ END CODE INSERT ~~~~
 
       # Create a new file in the cache for the requested file.
@@ -213,6 +219,11 @@ while True:
 
       # Save origin server response in the cache file
       # ~~~~ INSERT CODE ~~~~
+      
+      cacheFile.write(originServerResponse)
+      cacheFile.close()
+      print("origin server response cached")
+      
       # ~~~~ END CODE INSERT ~~~~
       cacheFile.close()
       print ('cache file closed')
